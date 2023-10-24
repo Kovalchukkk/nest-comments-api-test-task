a16997f6c134f4b4ef6988970c75132a5ffa602c
Mykola Kovalchuk <kolyankov@gmail.com>
2023-10-24 23:50:19 +0300

    Added user and roles endpoints

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, Length, IsEmail } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({ example: 'user@gmail.com', description: 'Email address' })
+  @IsString({ message: 'Must be a string' })
+  @IsEmail({}, { message: 'Invalid email' })
+  readonly email: string;
+  @ApiProperty({ example: '12345', description: 'Password' })
+  @IsString({ message: 'Must be a string' })
+  @Length(4, 16, { message: 'Must be greater than 4 and less than 16' })
+  readonly password: string;
+  @ApiProperty({ example: 'ice_cream', description: 'Username' })
+  @IsString({ message: 'Must be a string' })
+  @Length(4, 16, { message: 'Must be greater than 4 and less than 16' })
+  readonly username: string;
+}
---
+++ src/users/users.module.ts
@@ -0,0 +1,19 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AuthModule } from 'src/auth/auth.module';
+import { RolesModule } from 'src/roles/roles.module';
+import { UsersController } from './users.controller';
+import { User } from './users.model';
+import { UsersService } from './users.service';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  imports: [
+    SequelizeModule.forFeature([User]),
+    RolesModule,
+    forwardRef(() => AuthModule),
+  ],
+  exports: [UsersService],
+})
+export class UsersModule {}
