96e596c204509a844957b75a926d1ec8a49151bc
Mykola Kovalchuk <kolyankov@gmail.com>
2023-10-26 23:51:47 +0300

    Changed db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,11 +26,7 @@ import { AppGateway } from 'app.gateway';
     }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
+      uri: process.env.DATABASE_URL,
       models: [User, Role, UserRoles, Comment, Bookmark],
       autoLoadModels: true,
     }),
